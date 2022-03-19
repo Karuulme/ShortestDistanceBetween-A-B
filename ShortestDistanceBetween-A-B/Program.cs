ec3e87e83ee936bb84b6be4da16ff31b28c3a54b
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-03-19 17:21:10 +0300

    Update Program.cs
    Hatalar düzeltildi.

--- ShortestDistanceBetween-A-B/Program.cs
+++ ShortestDistanceBetween-A-B/Program.cs
@@ -7,21 +7,21 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             string[,] LabArray = new string[10, 10] {
-            {"A","0","0","0","0","A","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0","0","0"},
+            {"0","0","0","0","0","0","0","0","0","0"},
+            {"0","0","A","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0"},
-            {"0","0","0","0","0","0","0","0","0","0"},
-            {"0","0","0","0","0","0","0","0","0","0"},
-            {"0","0","0","0","0","0","0","0","0","0"},
-            {"0","0","0","0","0","0","0","B","0","0"},
+            {"0","0","0","0","0","0","B","0","0","0"},
             };
-            string[,] a=Patharray(LabArray, 0, 0, 9, 7);
-              LabArray[0,0] = "A";
-              LabArray[9, 7] = "B";
-            for (int i=0;i<10;i++)
+            string[,] a = Patharray(LabArray, 3, 2, 9, 6);
+            LabArray[3, 2] = "A";
+            LabArray[9, 6] = "B";
+            for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
@@ -30,33 +30,36 @@ namespace ConsoleApp5
                 Console.WriteLine("");
             }
         }
-        static string[,] Patharray(string[,] array,int Ai,int Aj, int Bi, int Bj)
+        static string[,] Patharray(string[,] array, int Ai, int Aj, int Bi, int Bj)
         {
             int
                 i = (Ai <= Bi) ? Ai : Bi,
                 j = (Ai <= Bi) ? Aj : Bj,
-                n = (Ai <= Bi) ? Bj : Aj;
+                n = (Ai <= Bi) ? Bj : Aj,
+                k = (Ai <= Bi) ? Bi : Ai;
             do
             {
                 i = (Ai == Bi) ? i : ++i;
                 j = (j >= n) ? --j : ++j;
                 array[i, j] = "X";
-                if (j == n || i == 9)
-                    if (Ai != Bi) ShortPatharray(array,i,j,Bi,Bj);
+                if (j == n || i == 9 || i == k)
+                    if (Ai != Bi) ShortPatharray(array, i, j, k, n);
             }
-            while (j != n && i < 10);
+            while (j != n && i < 10 && i < k);
             return array;
         }
-        static string[,] ShortPatharray(string[,] array,int Xi,int Xj,int Bi,int Bj)
+        static string[,] ShortPatharray(string[,] array, int Ai, int Aj, int Bi, int Bj)
         {
-           for (int i=(Xj == Bj) ? Xi : (Xj <= Bj) ? Xi : Bj,
-                n= (Xj == Bj) ?  Bi: (Xj <= Bj) ? Xi : Bj;
-                i!=n;i++
-                )
+            int
+                i = (Ai <= Bi) ? Ai : Bi, //  ai
+                j = (Ai <= Bi) ? Aj : Bj, //  aj
+                k = (Ai <= Bi) ? Bi : Ai, //  bi
+                n = (Ai <= Bi) ? Bj : Aj; //  bj
+            while (i != k || j != n)
             {
-                if (Xj == Bj)
-                    array[i, Bj] = "X";
-                else array[Bi, i] = "X";               
+                i = (Ai == Bi) ? i : ++i;
+                j = (Ai == Bi) ? (Aj <= Bj) ? ++j : --j : j;
+                array[i, j] = "X";
             }
             return array;
         }
